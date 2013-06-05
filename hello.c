4aa40c9df7c78be299f5fbd18da8a0f06a53ed15
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2013-06-05 13:06:29 +0530

    added code to subtract

--- hello.c
+++ hello.c
@@ -4,8 +4,8 @@ void main() {
     printf("Hello\n");
     printf("Good Bye, My job is done\n");
 	
-	printf("Sum of 3 and 5 is %d\n",3+5);
-
+    printf("Sum of 3 and 5 is %d\n",3+5);
+    printf("Difference of 10 and 4 is %d",10-4);
 
 }
 
